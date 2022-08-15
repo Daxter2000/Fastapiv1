dbeb5777afea7e93c372f6f20fb5cd9d17f90716
Alfredo <Alfredo.hdez@hotmail.com>
2022-08-15 11:34:07 -0600

    scripts to main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+    
+@app.get("/myname/{name}")
+async def myName(name: str):
+    return {"message": f"Hello {name}, this is my new API"}
+
